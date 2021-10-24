d14cdeda082d588737b1a6a606017c72b528bfe8
MorpDev <89537270+MorpDev@users.noreply.github.com>
2021-10-24 19:20:38 +0200

    Create kick.py

---
+++ kick.py
@@ -0,0 +1,5 @@
+@commands.command()
+@commands.has_permissions(kick_members=True)
+async def kick(ctx, member: discord.Member, *, reason=None):
+await client.kick(member)
+await ctx.send(f'User {member} sunucudan gönderildi')
